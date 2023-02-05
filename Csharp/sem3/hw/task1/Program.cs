08ba2f6176009dc7f164c2d3745cdbe3b03adea9
Naumov <naumovmk@yandex.ru>
2023-02-05 15:11:28 +0600

    add hw sem3

---
+++ Csharp/sem3/hw/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.Clear();
+Console.Write("Введите координаты первой точки по иксу: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты первой точки по игрику: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты первой точки по зед: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты второй точки по иксу: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты второй точки по игрику: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты второй точки по зед: ");
+int z2 = int.Parse(Console.ReadLine());
+double s = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine($"s={s:f3}");
