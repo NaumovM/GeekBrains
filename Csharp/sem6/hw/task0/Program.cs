be6b0c8839c145230cc8506397c6f89c20eaa7eb
Naumov <naumovmk@yandex.ru>
2023-02-15 21:15:10 +0600

    add hw sem6

---
+++ Csharp/sem6/hw/task0/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.Write("Введите числа через пробел: ");
+string[] st = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] FromStringToInt(string[] array) {
+    int[] intArray = new int[array.Length];
+    for (int i = 0; i < array.Length; i ++) {
+        intArray[i] = int.Parse(array[i]);
+    }
+    return intArray;
+}
+int[] intArray = FromStringToInt(st);
+int PositiveCount(int[] array) {
+    int count = 0;
+    foreach (var el in array) {
+        count += el > 0 ? 1 : 0;
+    }
+    return count;
+}
+Console.WriteLine($"Чисел больше 0: {PositiveCount(intArray)}");
---
+++ Csharp/sem6/hw/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+Console.Write("Введите b1 k1 b2 k2 через пробел: ");
+string[] st = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double b1 = double.Parse(st[0]), k1 = double.Parse(st[1]), b2 = double.Parse(st[2]), k2 = double.Parse(st[3]);
+double[] Coords(double b1, double k1, double b2, double k2) {
+    double[] coord = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    coord[0] = x;
+    double y = k2 * ((b2 - b1) / (k1 - k2)) + b2;
+    coord[1] = y;
+    return coord;
+}
+double[] coord = Coords(b1, k1, b2, k2);
+Console.WriteLine($"[{String.Join(", ", coord)}]");
+
+
+// y = k1 * x + b1
+// y = k2 * x + b2
+
+// y-y = k1 * x + b1 - (k2 * x + b2);
+// 0 = k1 * x - k2 * x + b1 - b2;
+// k1 * x - k2 * x  = b2 - b1;
+// (k1 - k2) * x = b2 - b1
+// x = ((b2 - b1) / (k1 - k2))
+// y = k2 * ((b2 - b1) / (k1 - k2)) + b2;
