28e8248d62ec1dca181aada8ca7c689a31b12d88
Naumov <naumovmk@yandex.ru>
2023-02-18 18:25:07 +0600

    add hw sem7

---
+++ Csharp/sem7/hw/task0/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+Console.Write("Введите количесвто строк: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количесвто столбцов: ");
+int columns = int.Parse(Console.ReadLine());
+double[,] GetArray(int rows, int columns, double minValue, double maxValue)
+{
+    double[,] res = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            res[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue; //непонятный принцип нахождения min max
+        }
+    }
+    return res;
+}
+double[,] array = GetArray(rows, columns, -9, 9);
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:0.0} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(array);
---
+++ Csharp/sem7/hw/task1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.Clear();
+Console.Write("Введите адрес элемента через пробел: ");
+string[] st = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int x = int.Parse(st[0]), y = int.Parse(st[1]);
+int rows = new Random().Next(3, 9);
+int columns = new Random().Next(3, 9); //rndm array
+int[,] GetArray(int rows, int columns)
+{
+    int[,] res = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            res[i, j] = new Random().Next(10); //rndm fill
+        }
+    }
+    return res;
+}
+int[,] array = GetArray(rows, columns);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(array);
+void PrintRes()
+{
+    if (x > array.GetLength(0) || y > array.GetLength(1))
+    {
+        Console.WriteLine($"Адрес элемента [{x}, {y}] -> не существует");
+    }
+    else Console.WriteLine($"Адрес элемента [{x}, {y}] -> {array[x - 1, y - 1]}");
+}
+PrintRes();
