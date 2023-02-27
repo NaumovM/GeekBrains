60ebb3e413fe11fa39ea51391bef6a5ca5d069f0
Naumov <naumovmk@yandex.ru>
2023-02-27 19:30:37 +0600

    add hw sem9

---
+++ Csharp/sem9/hw/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+// 4+5+6+7+8   
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+if (m > n)
+{
+    int t = n;
+    n = m;
+    m = t;
+}
+Console.Write(SumNumbers(m, n));
+
+int SumNumbers(int start, int end)
+{
+    while (start == end + 1) return 0;
+    return (start + SumNumbers(start + 1, end));
+}
---
+++ Csharp/sem9/hw/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Clear();
+Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int Akkerman(int n, int m) //логика функции не совсем понятна(после 3 оператора), логически переписана с мат. языка по формуле из гугла.
+{
+    if (n == 0) return m + 1;
+    else if (m == 0) return Akkerman(n - 1, 1);
+    else return (Akkerman((n - 1), Akkerman(n, m - 1)));
+}
+Console.WriteLine(Akkerman(m, n));
