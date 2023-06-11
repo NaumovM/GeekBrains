268cc229426d6808eb587c9ebad92091a6f7b8fa
NaumovM <naumovmk@yandex.ru>
2023-06-11 19:10:48 +0600

    add sem1 again

---
+++ Java/sem1/untitled/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        String s1 = "Hello World!";
+        System.out.println(s1);
+    }
+}
+ergegetg
