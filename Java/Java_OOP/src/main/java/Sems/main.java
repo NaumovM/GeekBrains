5d988d82c2ca6cc939559a9b07a8215a9435beba
NaumovM <naumovmk@yandex.ru>
2023-10-02 19:18:23 +0600

    new sem

---
+++ Java/Java_OOP/src/main/java/Sems/main.java
@@ -0,0 +1,30 @@
+package Sems;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        float number = getFloatInput();
+        System.out.println("Введенное число: " + number);
+    }
+
+    public static float getFloatInput() {
+        float number = 0.0f;
+        Scanner scanner = new Scanner(System.in);
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("Введите дробное число: ");
+            try {
+                number = scanner.nextFloat();
+                validInput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Ошибка! Введено некорректное значение. Повторите ввод.");
+                scanner.nextLine();
+            }
+        }
+
+        return number;
+    }
+}
