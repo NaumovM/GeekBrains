17852f3305911842d2fac65b4d7f687678f523d9
NaumovM <naumovmk@yandex.ru>
2024-02-19 13:29:48 +0600

    add sem1hw

---
+++ Java/java_core/src/main/java/pack/Class1.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class Class1 {
+    public void printMessage() {
+        System.out.println("This is Class1");
+    }
+}
---
+++ Java/java_core/src/main/java/pack/Class2.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class Class2 {
+    public void printMessage() {
+        System.out.println("This is Class2");
+    }
+}
